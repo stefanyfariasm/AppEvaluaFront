1c9bf4aee0b43a9ec53650585539ebed80d0e65b
Carlos Gomez <cdgt9522@gmail.com>
2023-06-14 12:28:06 -0500

    componente estilo

--- appEvalua/src/app/app.module.ts
+++ appEvalua/src/app/app.module.ts
@@ -5,18 +5,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './principal/principal.component';
 import { TipoBebidaComponent } from './tipo-bebida/tipo-bebida.component';
+import { EstilosComponent } from './estilos/estilos.component';
 
+
+/////////////
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+
+/////////////////
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalComponent,
-    TipoBebidaComponent
+    TipoBebidaComponent,
+    EstilosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCheckboxModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+}
